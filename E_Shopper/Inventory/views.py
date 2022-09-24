232655632c74ebbafd004d824ac27016d4fd19c2
imprachi <89079991+imprachi@users.noreply.github.com>
2022-09-24 14:39:37 +0530

    Update views.py

--- E_Shopper/Inventory/views.py
+++ E_Shopper/Inventory/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics, viewsets
-from rest_framework.pagination import PageNumberPagination
 from .models import Category, GST, Discount, Product
 from .serializers import CategorySerializer, GSTSerializer, DiscountSerializer, ProductSerializer
-from rest_framework.filters import SearchFilter,OrderingFilter
+from rest_framework.views import APIView
+from .models import Product
+from django.contrib import messages
+from tablib import Dataset
+from django.http import HttpResponse
+
 
 
 # Generic CRUD View for Category model
@@ -10,13 +14,13 @@ class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 
 
@@ -25,13 +29,13 @@ class GSTListCreateAPIView(generics.ListCreateAPIView):
     queryset = GST.objects.all()
     serializer_class = GSTSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 class GSTRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = GST.objects.all()
     serializer_class = GSTSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 
 
@@ -40,13 +44,13 @@ class DiscountListCreateAPIView(generics.ListCreateAPIView):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 class DiscountRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Discount.objects.all()
     serializer_class = DiscountSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
 
 
@@ -55,94 +59,28 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
-    filter_backends = [SearchFilter]
-    search_fields = ['p_name']
-    pagination_class = PageNumberPagination
-
-    #logic for stock quantity == reorder quantity
-    # def get_queryset(self, *args, **kwargs):
-    #     queryset = super(ProductListCreateAPIView, self).get_queryset(*args, **kwargs)
-    #     # queryset = Product.objects.all()
-    #     ps = self.request.GET.get('product_stock')                  #eg. 1000
-    #     ro = self.request.GET.get('reorder_level')                  #eg.   5
-    #     print("total quantity", ps)
-    #     print("remaining quantity", ro)
-    #     data = ps-2                              #CustomerOrder.objects.get('quantity'), data=1000-2
-    #     if data == ro:
-    #         print("Reorder level reached, please increase quantity of same product")
-    #     return queryset
+    # authentication_classes = []
 
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # permission_classes = []
-    # authentication_classes = [JWTAuthentication]
+    # authentication_classes = []
 
-
-
-
-
-
-
-
-
-import pandas as pd
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.conf import settings
-import uuid
-import os
-# from django.utils import timezone
-import datetime
-
-
-from .models import ExcelFileUpload
 class ExportImportExcel(APIView):
-	def get(self, request):
-		queryset = Product.objects.all()
-		serializer = ProductSerializer(queryset, many=True)
-		df = pd.DataFrame(serializer.data)
-		print("pandas dataframe",df)
-		# df.to_csv(f"{settings.BASE_DIR}/static/ExportProduct/{uuid.uuid4()}.csv", encoding='UTF-8', index=False)
-		df.to_csv(f"{settings.BASE_DIR}/static/ExportProduct/{datetime.datetime.now()}.csv", encoding='UTF-8', index=False)
-		return Response(status=status.HTTP_200_OK)
-	def post(self, request):
-		new_upload_file = request.FILES['files']
-		print("file type is file : ", new_upload_file)
-		exceled_upload_obj = ExcelFileUpload(excel_file_upload=new_upload_file)
-		print("exceled_upload_obj file upload : ", exceled_upload_obj)
-		df = pd.read_csv(f"{settings.BASE_DIR}/static/ImportProductPending/{exceled_upload_obj.excel_file_upload}", encoding='UTF-8')
-		print("list od data", df.values.tolist())
-		for i in df.values.tolist():
-			print(i)
-			print("Product id : ", i[0])
-			print("Product name : ", i[1])
-			print("Product Image : ", i[2])
-			print("Product price : ", i[3])
-			print("Product stock : ", i[4])
-			print("Product unit : ", i[5])
-			print("Product reorder level : ", i[6])
-			print("Product category : ", i[7])
-			print("Product vendor : ", i[8])
-			print("Product discount : ", i[9])
-			print("Product gst : ", i[10])
-			Product.objects.create(
-									p_id = i[0],
-									p_name = i[1],
-									category = i[2],
-									p_img = i[3],
-									p_price = i[4],
-									vendor = i[5],
-									product_stock = i[6],
-									unit = i[7],
-									reorder_level = i[8],
-									discount = i[9],
-									gst = i[10],
-									)
-			print(i)
-		return Response(status=status.HTTP_200_OK)
+
+    def get(self,request):
+        product_obj = Product.objects.all()
+        serializer = ProductSerializer(product_obj,many=True)
+        df = pd.dataframe(serializer.data)
+        print(df)
+
+        return Response({'status:200'})
+
+
+
+
+
 
 
 
